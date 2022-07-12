d2c273aabd29bcdb846a9fc36d88974bd9ef0fde
Maciej Jakubiak <maciej.z.jakubiak@gmail.com>
2022-07-12 09:41:46 -0500

    Week 4 Monday homework

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,11 @@
 class Actor < ApplicationRecord
+  validates :first_name, length: { minimum: 2 }, presence: true
+  validates :last_name, length: { minimum: 2 }, presence: true
+  validates :known_for, presence: true
+
+  def validate(actor)
+    if actor.age < 13
+      actor.errors.add :base, "This person is too young"
+    end
+  end
 end
